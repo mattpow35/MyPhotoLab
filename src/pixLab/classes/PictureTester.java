dbb5f3f1080b61231daf1868adb7ccb66e019ef3
mattpow35 <mpow2231@740s-pl-121.local>
2017-01-17 09:26:03 -0700

    commented out the test negate method

--- src/pixLab/classes/PictureTester.java
+++ src/pixLab/classes/PictureTester.java
@@ -103,7 +103,7 @@ public class PictureTester
     //testKeepOnlyBlue();
     //testKeepOnlyRed();
     //testKeepOnlyGreen();
-    testNegate();
+    //testNegate();
     //testGrayscale();
     //testFixUnderwater();
 	//testMirrorVertical();
